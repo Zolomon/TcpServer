5b8826992e75094278043a2b09d4e25c035ebcda
= <=>
2011-03-25 20:20:16 +0100

    Added some simple command line parameters to select client/server.

--- Networking/Main.cs
+++ Networking/Main.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Threading;
+using System.Net;
 
 namespace Networking
 {
@@ -7,7 +8,14 @@ namespace Networking
 	{
 		public static void Main (string[] args)
 		{
-			Server s = new Server();
+			Client c;
+			Server s;
+			//Server s = new Server();
+			if (args[0].Equals ("c")) {
+				c = new Client (args[1].ToString (), Convert.ToInt32 (args[2]));
+			} else if (args[0].Equals ("s")) {
+				s = new Server ();
+			}
 		}
 	}
 }
