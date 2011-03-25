789a39c49a6296cdb4b83e99606b85dfcced6442
= <=>
2011-03-25 21:32:31 +0100

    Server should be able to send message back to client.

--- Networking/Client.cs
+++ Networking/Client.cs
@@ -17,11 +17,24 @@ namespace Networking
 			using (TcpClient c = new TcpClient (address, portToListen)) {
 				
 				using (NetworkStream s = c.GetStream ()) {
-					BinaryWriter w = new BinaryWriter (s);
-					w.Write (msg);
-					w.Flush ();
-					BinaryReader r = new BinaryReader (s);
-					Console.WriteLine (r.ReadString ());
+//					BinaryWriter w = new BinaryWriter (s);
+//					w.Write (Console.ReadLine());
+//					w.Flush ();
+//					BinaryReader r = new BinaryReader (s);
+//					Console.WriteLine (r.ReadString ());
+					BinaryWriter w = new BinaryWriter(s);
+					BinaryReader r = new BinaryReader(s);
+					while (c.Connected) {
+						w.Write(Console.ReadLine());
+						w.Flush();
+						
+						data = new byte[1024];
+						recv = s.Read (data, 0, data.Length);
+						Console.WriteLine ("From {0}: {1}", client.Client.RemoteEndPoint.ToString (), Encoding.ASCII.GetString (data, 0, recv));
+						if (recv == 0) {
+							break;
+						}
+					}
 				}
 			}
 		}
